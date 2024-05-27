b5e01945975abd544b0e5520ab8bed31992eb80f
viscousguy <shubhamanchaliya84@gmail.com>
2024-05-27 19:26:46 +0530

    feat: minor refactor

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My React App</title>
+    <title>Crud App</title>
     @viteReactRefresh
     @vite('resources/js/app.jsx')
 </head>
